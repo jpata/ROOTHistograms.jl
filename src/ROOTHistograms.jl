46c0155133e1eac9e6bb8f447d5923b8d29e24e1
Joosep Pata <joosep.pata@gmail.com>
2015-09-06 13:11:22 +0200

    works with root6

--- src/ROOTHistograms.jl
+++ src/ROOTHistograms.jl
@@ -3,9 +3,7 @@ module ROOTHistograms
 using ROOT, Histograms
 
 import ROOT.to_root
-function to_root(h::NHistogram, name="hist")
-    length(size(h))==2 || error("to_root not defined for nd=$(length(size(h)))")
-
+function to_root{T, E}(h::ErrorHistogram{T, 2, E}, name="hist")
     nbins_x = nbins(h, 1)
     nbins_y = nbins(h, 2)
 
@@ -15,67 +13,67 @@ function to_root(h::NHistogram, name="hist")
     e1 = convert(Vector{Float64}, h.edges[1][2:end-1])
     e2 = convert(Vector{Float64}, h.edges[2][2:end-1])
 
-    hi = TH2D(name, name, int32(nbins_x - 3), pointer(e1), int32(nbins_y - 3), pointer(e2))
-    for i=0:nbins_x-1
-        for j=0:nbins_y-1
-            SetBinContent(hi, int32(i), int32(j), conts[i + 1, j + 1])
-            SetBinError(hi, int32(i), int32(j), errs[i + 1, j + 1])
+    hi = TH2D(name, name, Int32(nbins_x - 3), pointer(e1), Int32(nbins_y - 3), pointer(e2))
+    for i=0:nbins_x-2
+        for j=0:nbins_y-2
+            SetBinContent(hi, Int32(i), Int32(j), conts[i + 1, j + 1])
+            SetBinError(hi, Int32(i), Int32(j), errs[i + 1, j + 1])
         end
     end
     SetEntries(hi, sum(entries(h)))
     return hi
 end
 
-function to_root(h::Histogram, name="hist")
-    edges = copy(h.bin_edges)
+function to_root{T, E}(h::ErrorHistogram{T, 1, E}, name="hist")
 
     #remove underflow low and overflow high
-    edges = h.bin_edges[2:end-1]
-    hi = TH1D(name, name, int32(nbins(h) - 3), pointer(edges))
+    _edges = edges(h)[1][2:end-1]
+    hi = TH1D(name, name, Int32(nbins(h)[1] - 2), pointer(_edges))
     for i=0:GetNbinsX(hi)+1
-        SetBinContent(hi, int32(i), contents(h)[i + 1])
-        SetBinError(hi, int32(i), errors(h)[i + 1])
+        SetBinContent(hi, Int32(i), contents(h)[i + 1])
+        SetBinError(hi, Int32(i), errors(h)[i + 1])
     end
     SetEntries(hi, sum(entries(h)))
 
-    # println(h)
-    # Print(hi, "ALL")
-    conts, errs, ents, edges = get_hist_bins(hi)
-
-    if !all(conts .==  contents(h))
-        warn("mismatch in contents: \n", hcat(conts, contents(h))|>string, "\n")
-    end
-    if !all(errs .==  errors(h))
-        warn("mismatch in errors: \n", hcat(errs, errors(h))|>string, "\n")
-    end
-    # if !all(ents .==  entries(h))
-    #     warn("mismatch in entries: \n", hcat(ents, entries(h))|>string, "\n")
+    # # println(h)
+    # # Print(hi, "ALL")
+    # conts, errs, ents, _edges2 = get_hist_bins(hi)
+    # 
+    # if !all(conts .==  contents(h))
+    #     warn("mismatch in contents: \n", hcat(conts, contents(h))|>string, "\n")
     # end
-    if !all(edges .==  h.bin_edges)
-        warn("mismatch in edges: \n", hcat(edges, h.bin_edges)|>string, "\n")
-    end
+    # if !all(errs .==  errors(h))
+    #     warn("mismatch in errors: \n", hcat(errs, errors(h))|>string, "\n")
+    # end
+    # # if !all(ents .==  entries(h))
+    # #     warn("mismatch in entries: \n", hcat(ents, entries(h))|>string, "\n")
+    # # end
+    # if !all(_edges2 .==  _edges)
+    #     warn("mismatch in edges: \n", hcat(_edges, edges(h)[1])|>string, "\n")
+    # end
     return hi
 end
 
 function get_hist_bins(h::Union(TH1D, TH1A, TH1); error_type=:contents)
-    nb = int64(GetNbinsX(h))
+    nb = Int64(GetNbinsX(h))
     nb>0 || error("nbins = $nb")
 
     #+3 = underflow, overflow, superfluous extra bin
-    conts = zeros(Float64, nb+3)
-    errs = zeros(Float64, nb+3)
-    ents = zeros(Float64, nb+3)
+    conts = zeros(Float64, nb+2)
+    errs = zeros(Float64, nb+2)
+    ents = zeros(Float64, nb+2)
 
     #underflow low, overflow low, overflow high
     edges = zeros(Float64, nb+3)
     for n=0:nb+1
-        conts[n+1] = GetBinContent(h, int32(n))
-        errs[n+1] = GetBinError(h, int32(n))
+        #println("$n ", GetBinLowEdge(h, Int32(n)), " ", GetBinContent(h, Int32(n)), " ", GetBinError(h, Int32(n)))
+        conts[n+1] = GetBinContent(h, Int32(n))
+        errs[n+1] = GetBinError(h, Int32(n))
         #entries[n+1] = GetEntries(h) * conts[n+1]
-        edges[n+1] = GetBinLowEdge(h, int32(n))
+        edges[n+1] = GetBinLowEdge(h, Int32(n))
     end
 
-    #this work for histograms for which the bin errors have been manually set
+    #this works for histograms for which the bin errors have been manually set
     #to non-Poisson, GetEntries is meaningless
     if error_type == :errors
         ents = (conts ./ errs).^2
@@ -92,7 +90,7 @@ function get_hist_bins(h::Union(TH1D, TH1A, TH1); error_type=:contents)
     #ents = int(round(ents))
 
     edges[1] = -Inf
-    edges[nb+2] = edges[nb+1] + GetBinWidth(h, int32(nb))
+    edges[nb+2] = edges[nb+1] + GetBinWidth(h, Int32(nb))
     edges[nb+3] = Inf
 
     if error_type != :errors
@@ -101,7 +99,7 @@ function get_hist_bins(h::Union(TH1D, TH1A, TH1); error_type=:contents)
         end
     end
     if Integral(h)>0
-        @assert abs(sum(conts) - Integral(h, int32(0), int32(nb+1)))/sum(conts)<0.0001 string("contents unequal ", sum(conts), "!=", Integral(h, int32(0), int32(nb+1)))
+        @assert abs(sum(conts) - Integral(h, Int32(0), Int32(nb+1)))/sum(conts)<0.0001 string("contents unequal ", sum(conts), "!=", Integral(h, Int32(0), Int32(nb+1)))
     end
     # if (abs(sum(conts) - Integral(h)) > 100000 * eps(Float64))
     #     warn(
@@ -122,7 +120,9 @@ end
 
 function from_root(o::TH1A)
     conts, errs, ents, edgs = get_hist_bins(o)
-    h = Histogram(ents, conts, edgs)
+    h = ErrorHistogram{Float64, 1, Tuple{Vector{Float64}}}(
+        (edgs, ), conts, errs.^2, :left
+    )
     # for i=1:nbins(h)
     #     @assert abs(errors(h)[i] - errs[i])<0.0001 string("errors unequal $(hcat(errors(h), errs))")
     # end
@@ -132,22 +132,32 @@ end
 function from_root(o::TH2A)
     nx, ny = size(o)
     #println("nx=$nx ny=$ny")
-    arr = zeros(nx+3, ny+3)
-    ents = zeros(nx+3, ny+3)
+    conts = zeros(nx+2, ny+2)
+    errs = zeros(nx+2, ny+2)
+    edgs = (zeros(Float64, nx+3), zeros(Float64, ny+3))
+    for x=0:nx+1
+        edgs[1][x+1] = GetBinLowEdgeX(o, Int32(x))
+    end
+    for y=0:ny+1
+        edgs[2][y+1] = GetBinLowEdgeY(o, Int32(y))
+    end
+    edgs[1][1] = -Inf
+    edgs[1][end] = Inf
+    edgs[2][1] = -Inf
+    edgs[2][end] = Inf
     for x=0:nx+1
         for y=0:ny+1
-            arr[x+1, y+1] =
-                GetBinContent(root_cast(TH1, o), int32(x), int32(y))
+            conts[x+1, y+1] =
+                GetBinContent(root_cast(TH1, o), Int32(x), Int32(y))
+            errs[x+1, y+1] =
+                GetBinError(root_cast(TH1, o), Int32(x), Int32(y))
         end
     end
 
-    ents = arr .* GetEntries(o) ./ sum(arr)
-    ents[isnan(ents)] = 0
-    ents = round(ents)
-
-    return NHistogram(ents, arr, {[0:nx+2], [0:ny+2]})
+    return ErrorHistogram{Float64, 2, Tuple{Vector{Float64}, Vector{Float64}}}(
+        edgs, conts, errs.^2, :left
+    )
 end
-
 
 import Base.size
 Base.size(o::TH2D) = (GetNbinsX(root_cast(TH1, o)), GetNbinsY(root_cast(TH1, o)))
--- test/histroot.jl
+++ test/histroot.jl
@@ -1,38 +1,44 @@
 using ROOT, Histograms, ROOTHistograms
 using Base.Test
 
-h = Histogram([-Inf, 0,1,2,3,4, Inf])
+h = ErrorHistogram([-Inf, 0,1,2,3,4, Inf])
 for i=1:100
-    hfill!(h, 0.3)
+    push!(h, 0.3)
 end
 th = to_root(h, "hist1")
-@test from_root(th) == h
+_h = from_root(th)
+@test all(edges(h)[1] .== edges(_h)[1])
+@test all(contents(h) .== contents(_h))
+@test all(errors(h) .== errors(_h))
 
-h = Histogram([-Inf, 0,1,2,3,4, Inf])
+h = ErrorHistogram([-Inf, 0,1,2,3,4, Inf])
 for i=1:100
-    hfill!(h, 0.3, 0.1)
+    push!(h, 0.3, 0.1)
 end
 th = to_root(h, "hist2")
-@test from_root(th) == h
+_h = from_root(th)
+@test all(edges(h)[1] .== edges(_h)[1])
+@test all(contents(h) .== contents(_h))
+@test all(errors(h) .== errors(_h))
 
-
-nh = NHistogram({[-Inf, 0,1,2, Inf], [-Inf, 0,1,2, Inf]})
+nh = ErrorHistogram([-Inf, 0,1,2, Inf], [-Inf, 0,1,2, Inf])
 for i=1:100
-    hfill!(nh, {0.3, 0.3})
+    push!(nh, (0.3, 0.3))
 end
 th = to_root(nh, "nhist")
 nh2 = from_root(th)
-@test size(nh) == size(nh2)
-@test all(nh2.baseh.bin_entries .== nh.baseh.bin_entries)
-@test all(nh2.baseh.bin_contents .== nh.baseh.bin_contents)
+@test nbins(nh) == nbins(nh2)
+@test all(edges(nh)[1] .== edges(nh2)[1])
+@test all(edges(nh)[2] .== edges(nh2)[2])
+
+@test all(errors(nh) .== errors(nh2))
 @test all(contents(nh) .== contents(nh2))
-@test all(entries(nh) .== entries(nh2))
 
 tf = ROOT.TFile("tfile.root", "RECREATE")
-h = Histogram([-Inf, linspace(-1, 1, 30)..., Inf])
+h = ErrorHistogram([-Inf, linspace(-1, 1, 30)..., Inf])
 
 for i=1:1000000
-    hfill!(h, rand(-1:0.0001:1))
+    push!(h, rand(-1:0.0001:1))
 end
 th = to_root(h, "hist3")
 h2 = from_root(th)
@@ -44,6 +50,6 @@ h = hd["hist3"]
 
 ROOT.gROOT.process_line("TFile* tf = new TFile(\"tfile.root\");")
 ROOT.gROOT.process_line("TH1D* h = (TH1D*)(tf->Get(\"hist3\"));")
-for i=1:nbins(h)
+for i=1:nbins(h)[1]
     @test contents(h)[i] == ROOT.gROOT.process_line("h->GetBinContent($(i-1));")
 end
